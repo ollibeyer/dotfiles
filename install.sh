648c4499661cae55f7084d819ea2a24704dbf203
Oliver Beyer <oliver.beyer@mail.de>
2026-06-20 13:26:31 +0200

    added starship config

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Lösche alte Dateien und setze Symlinks
 ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml 
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 echo "Alle Konfigurationen wurden verlinkt!"
