affa02de5d3969b4d45d2b230cb8e615c686898c
Oliver Beyer <oliver.beyer@mail.de>
2026-07-09 19:55:29 +0200

    added comments and changed theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,10 @@
 require("settings")
 
+-- Set leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Enable filetype detection, plugin, and indent
 vim.cmd([[
   filetype plugin indent on
 ]])
@@ -10,17 +12,18 @@ vim.g.copilot_filetypes = {
   ["*"] = true,
 }
 
-
+-- Bootstrap lazy.nvim if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Setup lazy.nvim with plugins
 require("lazy").setup({
   spec = { 
     { import = "plugins" },
-    { "folke/tokyonight.nvim", priority = 1000, config = function() vim.cmd.colorscheme("tokyonight-night") end },
+    { "folke/tokyonight.nvim", priority = 1000, config = function() vim.cmd.colorscheme("tokyonight") end },
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     { "nvim-tree/nvim-tree.lua", config = true },
    },
@@ -42,5 +45,6 @@ vim.defer_fn(function()
   end
 end, 200)
 
+-- Set options
 vim.opt.number = true
 vim.opt.termguicolors = true
